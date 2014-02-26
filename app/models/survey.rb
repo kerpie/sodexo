b35a8c5127620614cd9d598f12e74f796a9d394f
Kerry Perez <k4torce@gmail.com>
2014-02-26 01:27:47 -0500

    changes for android app

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,7 +1,7 @@
 class Survey < ActiveRecord::Base
 
 	belongs_to :restaurant
-	has_many :choosen_questions
+	has_many :choosen_questions, dependent: :destroy
 	has_many :availabilities, through: :choosen_questions
 
 end
