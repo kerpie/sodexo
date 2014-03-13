eb13f18a33abf34a0939eec40a76596f8a92f184
Kerry Perez <k4torce@gmail.com>
2014-03-13 07:23:52 -0500

    create survey done

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,8 @@
 class AnswersController < ApplicationController
   before_action :set_answer, only: [:show, :edit, :update, :destroy]
 
+  before_filter :authenticate_user!, except: :create
+
   # GET /answers
   # GET /answers.json
   def index
