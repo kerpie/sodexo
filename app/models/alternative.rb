d4c7fb62c3530208e55cc7785e695e019a9bfac0
Kerry Thomas Perez Huanca <kerry@MacBook-Pro-de-Kerry.local>
2015-02-18 11:49:54 -0500

    Modificaciones para nueva conexion local
    ;
    exit

--- app/models/alternative.rb
+++ app/models/alternative.rb
@@ -2,4 +2,5 @@ class Alternative < ActiveRecord::Base
 	
 	belongs_to :question
 	has_many :answers	
+	
 end
--- app/models/choosen_question.rb
+++ app/models/choosen_question.rb
@@ -3,4 +3,5 @@ class ChoosenQuestion < ActiveRecord::Base
 	belongs_to :survey
 	belongs_to :availability
 	has_many :answers
+
 end
